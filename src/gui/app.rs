a83b867429d5ae3b3e52559147bbbce807bc62ff
Martan03 <martinslezak03@gmail.com>
2023-09-10 22:58:41 +0200

    Iced gui test

---
+++ src/gui/app.rs
@@ -0,0 +1,48 @@
+use iced::widget::{Button, Column, Container, Text};
+use iced::Sandbox;
+use iced::Settings;
+
+#[derive(Debug)]
+pub struct Counter {
+    count: i32,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum CounterMessage {
+    Increment,
+    Decrement,
+}
+
+impl Sandbox for Counter {
+    type Message = CounterMessage;
+
+    fn new() -> Self {
+        Counter { count: 0 }
+    }
+
+    fn title(&self) -> String {
+        String::from("BUMP")
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            CounterMessage::Increment => self.count += 1,
+            CounterMessage::Decrement => self.count -= 1,
+        }
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        let label = Text::new(format!("Count: {}", self.count));
+        let incr = Button::new("Increment").on_press(CounterMessage::Increment);
+        let decr = Button::new("Decrement").on_press(CounterMessage::Decrement);
+
+        let col = Column::new().push(incr).push(label).push(decr);
+
+        Container::new(col)
+            .center_x()
+            .center_y()
+            .width(iced::Length::Fill)
+            .height(iced::Length::Fill)
+            .into()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use gui::app::Counter;
+use iced::widget::{Button, Column, Container, Text};
+use iced::Sandbox;
+use iced::Settings;
+
+mod gui {
+    pub mod app;
+}
+
+fn main() -> Result<(), iced::Error> {
+    Counter::run(Settings::default())
 }
