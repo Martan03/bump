8b27647ee2ca7df2bdc90b162caf31dadde49bc3
Martan03 <martinslezak03@gmail.com>
2023-09-12 00:02:49 +0200

    Add option to skip songs using counter

--- src/gui/app.rs
+++ src/gui/app.rs
@@ -1,4 +1,4 @@
-use iced::widget::{button, column, text};
+use iced::widget::{button, column, text, Text, Column};
 use iced::{executor, Alignment, Application, Command, Element, Renderer};
 
 use crate::config::config::Config;
@@ -6,7 +6,7 @@ use crate::library::library::Library;
 use crate::player::player::Player;
 
 pub struct BumpApp {
-    count: i32,
+    count: usize,
     player: Player,
     library: Library,
     config: Config,
@@ -46,10 +46,28 @@ impl Application for BumpApp {
         match message {
             BumpMessage::Increment => {
                 self.count += 1;
+                let songs = self.library.get_songs();
+                self.count %= songs.len();
+                let playing = self.player.is_playing();
+                _ = self.player.load(
+                    songs[self.count].get_path(),
+                    playing
+                );
             },
-            BumpMessage::Decrement => self.count -= 1,
+            BumpMessage::Decrement => {
+                let songs = self.library.get_songs();
+                if self.count > 0 {
+                    self.count -= 1;
+                } else {
+                    self.count = songs.len() - 1;
+                }
+                let playing = self.player.is_playing();
+                _ = self.player.load(
+                    songs[self.count].get_path(),
+                    playing
+                );
+            },
             BumpMessage::Play(play) => {
-                self.library.find(&mut self.config);
                 let playing = self.player.is_playing();
                 _ = self.player.play(play.unwrap_or(!playing));
             },
@@ -70,10 +88,30 @@ impl Application for BumpApp {
             button("Increment").on_press(BumpMessage::Increment),
             text(self.count).size(50),
             button("Decrement").on_press(BumpMessage::Decrement),
-            button("Play").on_press(BumpMessage::Play(None))
+            button("Play").on_press(BumpMessage::Play(None)),
+            self.vector_display(),
         ]
         .padding(20)
         .align_items(Alignment::Center)
         .into()
+    }
+}
+
+impl BumpApp {
+    fn vector_display(&self) -> Element<BumpMessage> {
+        let songs = self.library.get_songs();
+        let song_elements: Vec<Element<_>> = songs.iter().map(|song| {
+            Text::new(song.get_name()).into()
+        }).collect();
+    
+        // Combine the elements into a single element using the + operator
+        let combined_elements: Element<BumpMessage> = song_elements
+            .into_iter()
+            .fold(Column::new().spacing(20), |column, element| {
+                column.push(element)
+            })
+            .into();
+    
+        combined_elements
     }
 }
