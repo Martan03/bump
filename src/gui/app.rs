d043d3b10286694b1e28490f9b5f172ce8426bb3
Martan03 <martinslezak03@gmail.com>
2023-09-11 11:08:25 +0200

    Add player functions

--- src/gui/app.rs
+++ src/gui/app.rs
@@ -1,22 +1,34 @@
 use iced::widget::{button, column, text};
-use iced::{Alignment, Element, Sandbox, Settings};
+use iced::{Alignment, Element, Sandbox};
 
-#[derive(Debug)]
+use crate::player::player::Player;
+
 pub struct Counter {
     count: i32,
+    player: Player
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum PlayMessage {
+    Play,
+    Pause,
 }
 
 #[derive(Debug, Clone, Copy)]
 pub enum CounterMessage {
     Increment,
     Decrement,
+    Play(PlayMessage)
 }
 
 impl Sandbox for Counter {
     type Message = CounterMessage;
 
     fn new() -> Self {
-        Counter { count: 0 }
+        Counter {
+            count: 0,
+            player: Player::new()
+        }
     }
 
     fn title(&self) -> String {
@@ -27,14 +39,17 @@ impl Sandbox for Counter {
         match message {
             CounterMessage::Increment => self.count += 1,
             CounterMessage::Decrement => self.count -= 1,
-        }
+            CounterMessage::Play(PlayMessage::Play) => _ = self.player.play(),
+            CounterMessage::Play(PlayMessage::Pause) => _ = self.player.togglePlay(),
+        };
     }
 
     fn view(&self) -> Element<CounterMessage> {
         column![
             button("Increment").on_press(CounterMessage::Increment),
             text(self.count).size(50),
-            button("Decrement").on_press(CounterMessage::Decrement)
+            button("Decrement").on_press(CounterMessage::Decrement),
+            button("Play").on_press(CounterMessage::Play(PlayMessage::Play))
         ]
         .padding(20)
         .align_items(Alignment::Center)
--- src/player/player.rs
+++ src/player/player.rs
@@ -1,15 +1,58 @@
 // Hi Bonny4, I'm using your library
 // There's so much, I know! But I'm tired now
 
-use raplay::Sink;
+use std::fs::File;
+
+use eyre::Result;
+use raplay::{Sink, source::{Symph, symph::SymphOptions}};
+
+pub enum PlayState {
+    NotPlaying,
+    Playing,
+    Paused
+}   
 
 pub struct Player {
-    sink: Sink
+    sink: Sink,
+    symph: SymphOptions,
+    state: PlayState
 }
 
 impl Player {
     pub fn new() -> Self {
-        Player { sink: Sink::default() }
+        Player { 
+            sink: Sink::default(),
+            symph: SymphOptions::default(),
+            state: PlayState::NotPlaying
+        }
+    }
+
+    pub fn load(&mut self, path: String) -> Result<()> {
+        let file = File::open(path)?;
+        let src = Symph::try_new(file, &self.symph)?;
+        self.sink.load(src, true)?;
+        Ok(())
+    }
+
+    pub fn togglePlay(&mut self) -> Result<()> {
+        match self.state {
+            PlayState::Playing => {
+                self.state = PlayState::Paused;
+                self.sink.pause()?;
+            }
+            _ => {
+                self.state = PlayState::Playing;
+                self.sink.resume()?;
+            }
+        }
+        Ok(())
+    }
+
+    pub fn play(&mut self) -> Result<()> {
+        print!("test");
+        self.load("/home/martan03/Music/Imagine Dragons - Mercury - Act 1/".to_owned())?;
+        self.togglePlay()?;
+        Ok(())
     }
 }
 
