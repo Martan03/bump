3e9e1066d6fd43b4c11194b8b85953e34d4f45fd
Martan03 <martinslezak03@gmail.com>
2023-09-23 19:53:07 +0200

    Add option to control player from cli

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use std::env;
+use std::{
+    io::prelude::*,
+    net::TcpStream, env,
+};
+
 
 use config::config::Config;
 use gui::app::BumpApp;
+use gui::app::Msg;
+use gui::app::PlayerMsg;
 use gui::gui::Gui;
 use iced::window;
 use iced::window::PlatformSpecific;
@@ -35,6 +41,11 @@ mod player {
 }
 
 fn main() -> Result<(), iced::Error> {
+    let args: Vec<_> = env::args().skip(1).collect();
+    if !args.is_empty() {
+        parse_args(args);
+        return Ok(());
+    }
     // on wayland, the app freezes when minimized, this is temporary workaround
     // until it is fixed
     env::set_var("WINIT_UNIX_BACKEND", "x11");
@@ -87,5 +98,54 @@ fn make_settings() -> Settings<(Config, Gui)> {
         exit_on_close_request: false,
         flags: (config, gui),
         ..Default::default()
+    }
+}
+
+/// Parses given arguments
+fn parse_args(mut args: Vec<String>) {
+    if let Some(arg) = args.get(0) {
+        if arg == "h" || arg == "-h" || arg == "--help" {
+            help();
+        } else if arg == "i" || arg == "instance" {
+            args.remove(0);
+            if args.is_empty() {
+                eprintln!("No instance arguments given");
+            } else {
+                parse_instance_args(args);
+            }
+        } else {
+            eprintln!("Invalid argument: {arg}");
+            return;
+        }
+    }
+}
+
+/// Prints help
+/// TODO
+fn help() {
+    println!("This will be help one day");
+}
+
+/// Parses instance arguments
+fn parse_instance_args(args: Vec<String>) {
+    for arg in args {
+        if arg == "pp" || arg == "play-pause" {
+            send_message(Msg::Plr(PlayerMsg::Play(None)));
+        } else if arg == "next" {
+            send_message(Msg::Plr(PlayerMsg::Next));
+        } else if arg == "prev" {
+            send_message(Msg::Plr(PlayerMsg::Prev));
+        } else {
+            println!("Invalid argument: {arg}");
+        }
+    }
+}
+
+/// Sends given message to the server
+fn send_message(msg: Msg) {
+    if let Ok(mut stream) = TcpStream::connect("127.0.0.1:2867") {
+        if let Ok(msg) = serde_json::to_string::<Msg>(&msg) {
+            _ = stream.write(msg.as_bytes());
+        }
     }
 }
