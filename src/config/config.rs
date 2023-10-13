3e74fe78c3b84e8850bfe67e06db0cc92afaca66
Martan03 <martinslezak03@gmail.com>
2023-10-13 21:50:10 +0200

    Edit config to use generate_struct macro

--- src/config/config.rs
+++ src/config/config.rs
@@ -7,57 +7,50 @@ use std::{
     path::PathBuf,
     time::Duration,
 };
+use place_macro::place;
 
-use crate::gui::app::{BumpApp, ConfMsg};
+use crate::{gui::app::{BumpApp, ConfMsg}, generate_struct};
 
-#[derive(Clone, Serialize, Deserialize)]
-pub struct Config {
-    /// Paths where songs are saved
-    #[serde(default = "Config::get_default_song_paths")]
-    paths: Vec<PathBuf>,
-    /// Valid file extensions
-    #[serde(default = "Config::get_default_extensions")]
-    extensions: Vec<String>,
-    /// Path to the library
-    #[serde(default = "Config::get_default_library_path")]
-    library_path: PathBuf,
-    /// Path to the gui state json
-    #[serde(default = "Config::get_default_gui_path")]
-    gui_path: PathBuf,
-    /// Path to the player file
-    #[serde(default = "Config::get_default_player_path")]
-    player_path: PathBuf,
-    /// Whether it should use recursive search when finding songs
-    #[serde(default = "Config::get_default_recursive_search")]
-    recursive_search: bool,
-    /// When true shuffles currently playing song as well
-    #[serde(default = "Config::get_default_shuffle_current")]
-    shuffle_current: bool,
-    /// Fade length of the playback when pausing
-    #[serde(default = "Config::get_default_fade")]
-    fade: Duration,
-    /// When true automatically starts playing last played song after start
-    #[serde(default = "Config::get_default_autoplay")]
-    autoplay: bool,
-    /// When true loads new songs on app start
-    #[serde(default = "Config::get_default_start_load")]
-    start_load: bool,
-    /// When true plays songs without the gaps in between
-    #[serde(default = "Config::get_default_gapless")]
-    gapless: bool,
-    /// IP of the server
-    #[serde(default = "Config::get_default_server_ip")]
-    server_ip: String,
-    /// Port of the server
-    #[serde(default = "Config::get_default_server_port")]
-    server_port: String,
-    /// App hotkeys
-    #[serde(default = "Config::get_default_hotkeys")]
-    hotkeys: HashMap<String, String>,
-    enable_hotkeys: bool,
-    /// True when anything in config changed, else false
-    #[serde(skip, default)]
-    changed: bool,
+generate_struct! {
+    pub Config {
+        paths: Vec<PathBuf> => {
+            if let Some(dir) = dirs::audio_dir() {
+                vec![dir]
+            } else {
+                vec![PathBuf::from(".")]
+            }
+        },
+        extensions: Vec<String> => {
+            vec![
+                "mp3".to_owned(),
+                "flac".to_owned(),
+                "m4a".to_owned(),
+                "mp4".to_owned(),
+            ]
+        },
+        library_path: PathBuf => Config::get_config_dir().join("library.json"),
+        gui_path: PathBuf => Config::get_config_dir().join("gui.json"),
+        player_path: PathBuf => Config::get_config_dir().join("player.json"),
+        server_ip: String => "127.0.0.1".to_owned(),
+        server_port: String => "2867".to_owned(),
+        hotkeys: HashMap<String, String> => {
+            let mut hotkeys = HashMap::new();
+            hotkeys.insert("ctrl+alt+home".to_owned(), "pp".to_owned());
+            hotkeys.insert("ctrl+alt+pg_up".to_owned(), "prev".to_owned());
+            hotkeys.insert("ctrl+alt+pg_down".to_owned(), "next".to_owned());
+            hotkeys.insert("ctrl+alt+up".to_owned(), "vu".to_owned());
+            hotkeys.insert("ctrl+alt+down".to_owned(), "vd".to_owned());
+            hotkeys
+        },
+        ;
+        fade: Duration => Duration::from_millis(150),
+        recursive_search: bool => true,
+        shuffle_current: bool => false,
+        autoplay: bool => false,
+        start_load: bool => true,
+        gapless: bool => false,
+        enable_hotkeys: bool => true,
+    }
 }
 
 impl Config {
@@ -100,11 +93,6 @@ impl Config {
     ///                          Getters & Setters                          ///
     ///>===================================================================<///
 
-    /// Gets all paths songs are saved in
-    pub fn get_paths(&self) -> &Vec<PathBuf> {
-        &self.paths
-    }
-
     /// Adds given path to paths
     pub fn add_path(&mut self, path: PathBuf) {
         self.changed = true;
@@ -119,26 +107,6 @@ impl Config {
         }
     }
 
-    /// Gets library path
-    pub fn get_library_path(&self) -> &PathBuf {
-        &self.library_path
-    }
-
-    /// Gets gui path
-    pub fn get_gui_path(&self) -> &PathBuf {
-        &self.gui_path
-    }
-
-    /// Gets player path
-    pub fn get_player_path(&self) -> &PathBuf {
-        &self.player_path
-    }
-
-    /// Gets valid extensions
-    pub fn get_extensions(&self) -> &Vec<String> {
-        &self.extensions
-    }
-
     /// Gets config dir path
     pub fn get_config_dir() -> PathBuf {
         if let Some(mut dir) = dirs::config_dir() {
@@ -149,218 +117,32 @@ impl Config {
         }
     }
 
-    /// Gets shuffle current
-    pub fn get_shuffle_current(&self) -> bool {
-        self.shuffle_current
-    }
-
-    /// Sets shuffle current to given value
-    pub fn set_shuffle_current(&mut self, val: bool) {
-        self.changed = true;
-        self.shuffle_current = val;
-    }
-
-    /// Gets whether songs finder should use recursive search
-    pub fn get_recursive_search(&self) -> bool {
-        self.recursive_search
-    }
-
-    /// Sets recursive search to given value
-    pub fn set_recursive_search(&mut self, val: bool) {
-        self.changed = true;
-        self.recursive_search = val;
-    }
-
-    /// Gets fade length
-    pub fn get_fade(&self) -> Duration {
-        self.fade
-    }
-
-    /// Gets whether song should start playing on start
-    pub fn get_autoplay(&self) -> bool {
-        self.autoplay
-    }
-
-    /// Sets autoplay to given value
-    pub fn set_autoplay(&mut self, val: bool) {
-        self.changed = true;
-        self.autoplay = val;
-    }
-
-    /// Gets whether songs should load on start
-    pub fn get_start_load(&self) -> bool {
-        self.start_load
-    }
-
-    /// Sets start load to given value
-    pub fn set_start_load(&mut self, val: bool) {
-        self.changed = true;
-        self.start_load = val;
-    }
-
-    /// Gets whether gapless is enabled
-    pub fn get_gapless(&self) -> bool {
-        self.gapless
-    }
-
-    /// Sets gapless to given value
-    pub fn set_gapless(&mut self, val: bool) {
-        self.changed = true;
-        self.gapless = val;
-    }
-
-    /// Gets server ip
-    pub fn get_server_ip(&self) -> &str {
-        &self.server_ip
-    }
-
-    /// Gets server port
-    pub fn get_server_port(&self) -> &str {
-        &self.server_port
-    }
-
     /// Gets server address
     pub fn get_server_address(&self) -> String {
         format!("{}:{}", self.server_ip, self.server_port)
-    }
-
-    /// Gets hotkeys
-    pub fn get_hotkeys(&self) -> &HashMap<String, String> {
-        &self.hotkeys
-    }
-
-    /// Gets enable hotkeys
-    pub fn get_enable_hotkeys(&self) -> bool {
-        self.enable_hotkeys
-    }
-
-    pub fn set_enable_hotkeys(&mut self, val: bool) {
-        self.changed = true;
-        self.enable_hotkeys = val;
-    }
-
-    ///>===================================================================<///
-    ///                        Default Config values                        ///
-    ///>===================================================================<///
-
-    /// Gets default songs path
-    fn get_default_song_paths() -> Vec<PathBuf> {
-        if let Some(dir) = dirs::audio_dir() {
-            vec![dir]
-        } else {
-            vec![PathBuf::from(".")]
-        }
-    }
-
-    /// Gets default path to library
-    fn get_default_library_path() -> PathBuf {
-        Config::get_config_dir().join("library.json")
-    }
-
-    /// Gets default path to gui state file
-    fn get_default_gui_path() -> PathBuf {
-        Config::get_config_dir().join("gui.json")
-    }
-
-    /// Gets default path to player state file
-    fn get_default_player_path() -> PathBuf {
-        Config::get_config_dir().join("player.json")
-    }
-
-    /// Gets default extensions list
-    fn get_default_extensions() -> Vec<String> {
-        vec![
-            "mp3".to_owned(),
-            "flac".to_owned(),
-            "m4a".to_owned(),
-            "mp4".to_owned(),
-        ]
-    }
-
-    /// Gets default recursive search
-    fn get_default_recursive_search() -> bool {
-        true
-    }
-
-    /// Gets default shuffle current
-    fn get_default_shuffle_current() -> bool {
-        false
-    }
-
-    /// Gets default fade length
-    fn get_default_fade() -> Duration {
-        Duration::from_millis(150)
-    }
-
-    /// Gets default autoplay value
-    fn get_default_autoplay() -> bool {
-        false
-    }
-
-    /// Gets default start load
-    fn get_default_start_load() -> bool {
-        true
-    }
-
-    /// Gets defautl gapless
-    fn get_default_gapless() -> bool {
-        false
-    }
-
-    /// Gets default server ip
-    fn get_default_server_ip() -> String {
-        "127.0.0.1".to_owned()
-    }
-
-    /// Gets default server port
-    fn get_default_server_port() -> String {
-        "2867".to_owned()
-    }
-
-    /// Gets default hotkeys
-    fn get_default_hotkeys() -> HashMap<String, String> {
-        let mut hotkeys = HashMap::new();
-        hotkeys.insert("ctrl+alt+home".to_owned(), "pp".to_owned());
-        hotkeys.insert("ctrl+alt+pg_up".to_owned(), "prev".to_owned());
-        hotkeys.insert("ctrl+alt+pg_down".to_owned(), "next".to_owned());
-        hotkeys.insert("ctrl+alt+up".to_owned(), "vu".to_owned());
-        hotkeys.insert("ctrl+alt+down".to_owned(), "vd".to_owned());
-        hotkeys
-    }
-
-    /// Gets default enable hotkeys
-    fn get_default_enable_hotkeys() -> bool {
-        true
     }
 }
 
 /// Implements default for Config
 impl Default for Config {
     fn default() -> Self {
-        let mut library_path = Config::get_config_dir();
-        let mut gui_path = library_path.clone();
-        let mut player_path = library_path.clone();
-        library_path.push("library.json");
-        gui_path.push("gui.json");
-        player_path.push("player.json");
-
         Config {
-            paths: Config::get_default_song_paths(),
-            extensions: Config::get_default_extensions(),
-            library_path: Config::get_default_library_path(),
-            gui_path: Config::get_default_gui_path(),
-            player_path: Config::get_default_player_path(),
-            recursive_search: Config::get_default_recursive_search(),
-            shuffle_current: Config::get_default_shuffle_current(),
-            fade: Config::get_default_fade(),
-            autoplay: Config::get_default_autoplay(),
-            start_load: Config::get_default_start_load(),
-            gapless: Config::get_default_gapless(),
+            paths: Config::default_paths(),
+            extensions: Config::default_extensions(),
+            library_path: Config::default_library_path(),
+            gui_path: Config::default_gui_path(),
+            player_path: Config::default_player_path(),
+            recursive_search: Config::default_recursive_search(),
+            shuffle_current: Config::default_shuffle_current(),
+            fade: Config::default_fade(),
+            autoplay: Config::default_autoplay(),
+            start_load: Config::default_start_load(),
+            gapless: Config::default_gapless(),
             changed: true,
-            server_ip: Config::get_default_server_ip(),
-            server_port: Config::get_default_server_port(),
-            hotkeys: Config::get_default_hotkeys(),
-            enable_hotkeys: Config::get_default_enable_hotkeys(),
+            server_ip: Config::default_server_ip(),
+            server_port: Config::default_server_port(),
+            hotkeys: Config::default_hotkeys(),
+            enable_hotkeys: Config::default_enable_hotkeys(),
         }
     }
 }
--- src/core/macros.rs
+++ src/core/macros.rs
@@ -1,8 +1,12 @@
+#[macro_export]
 macro_rules! generate_struct {
     (
         $access:vis $name:ident {
             $( $var_access:vis $var_name:ident: $var_type:ty
                 $(=> $var_default:expr)?,
+            )*;
+            $( $ref_access:vis $ref_name:ident: $ref_type:ty
+                $(=> $ref_default:expr)?,
             )*
         }
     ) => {
@@ -17,6 +21,15 @@ macro_rules! generate_struct {
                         ))]
                     )?
                     $var_access $var_name: $var_type,
+                )*
+                $(
+                    $(
+                        __ignore__($ref_default)
+                        #[serde(default = __string__(
+                            $name "::default_" $ref_name
+                        ))]
+                    )?
+                    $ref_access $ref_name: $ref_type,
                 )*
                 changed: bool,
             }
@@ -35,6 +48,22 @@ macro_rules! generate_struct {
                     $(
                         fn __ident__("default_" $var_name)() -> $var_type {
                             $var_default
+                        }
+                    )?
+                )*
+                $(
+                    pub fn __ident__("get_" $ref_name)(&self) -> $ref_type {
+                        self.$ref_name
+                    }
+
+                    pub fn __ident__("set_" $ref_name)(&mut self, value: $ref_type) {
+                        self.changed = true;
+                        self.$ref_name = value;
+                    }
+
+                    $(
+                        fn __ident__("default_" $ref_name)() -> $ref_type {
+                            $ref_default
                         }
                     )?
                 )*
