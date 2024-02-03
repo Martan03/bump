e46991631cc47b222d89c80a5c96cde7e70d905a
Martan03 <martinslezak03@gmail.com>
2024-02-03 13:42:36 +0100

    Make settings scrollable

--- src/gui/settings/settings.rs
+++ src/gui/settings/settings.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
 use iced::{
-    widget::{button, column, text},
+    widget::{button, column, scrollable, text},
     Command, Renderer,
 };
 use iced_core::Length;
@@ -23,49 +23,51 @@ type Element<'a> = iced::Element<'a, Msg, Renderer<Theme>>;
 
 impl BumpApp {
     pub fn view_settings(&self) -> Element {
-        column![
-            text("Settings").size(25).style(Text::Normal),
-            button("Update library")
-                .on_press(Msg::Lib(LibMsg::LoadStart))
-                .style(Button::Primary),
-            text("Songs loading:").height(22).style(Text::Normal),
-            toggler(
-                "Update library on start".to_owned(),
-                self.config.get_start_load(),
-                |val| Msg::Conf(ConfMsg::StartLoad(val))
-            ),
-            toggler(
-                "Recursive search for songs".to_owned(),
-                self.config.get_recursive_search(),
-                |val| Msg::Conf(ConfMsg::RecursiveSearch(val))
-            ),
-            self.get_paths_input(),
-            text("Playback:").height(22).style(Text::Normal),
-            toggler(
-                "Shuffle currently playing song".to_owned(),
-                self.config.get_shuffle_current(),
-                |val| Msg::Conf(ConfMsg::ShuffleCurrent(val))
-            ),
-            toggler(
-                "Automatically start playing song on start".to_owned(),
-                self.config.get_autoplay(),
-                |val| Msg::Conf(ConfMsg::Autoplay(val))
-            ),
-            toggler(
-                "Play songs without gap between them".to_owned(),
-                self.config.get_gapless(),
-                |val| Msg::Conf(ConfMsg::Gapless(val))
-            ),
-            text("Hotkeys:").height(22).style(Text::Normal),
-            toggler(
-                "Enable hotkeys".to_owned(),
-                self.config.get_enable_hotkeys(),
-                |val| Msg::Conf(ConfMsg::EnableHotkeys(val))
-            ),
-        ]
-        .width(Length::Fill)
-        .spacing(2)
-        .padding(3)
+        scrollable(
+            column![
+                text("Settings").size(25).style(Text::Normal),
+                button("Update library")
+                    .on_press(Msg::Lib(LibMsg::LoadStart))
+                    .style(Button::Primary),
+                text("Songs loading:").height(22).style(Text::Normal),
+                toggler(
+                    "Update library on start".to_owned(),
+                    self.config.get_start_load(),
+                    |val| Msg::Conf(ConfMsg::StartLoad(val))
+                ),
+                toggler(
+                    "Recursive search for songs".to_owned(),
+                    self.config.get_recursive_search(),
+                    |val| Msg::Conf(ConfMsg::RecursiveSearch(val))
+                ),
+                self.get_paths_input(),
+                text("Playback:").height(22).style(Text::Normal),
+                toggler(
+                    "Shuffle currently playing song".to_owned(),
+                    self.config.get_shuffle_current(),
+                    |val| Msg::Conf(ConfMsg::ShuffleCurrent(val))
+                ),
+                toggler(
+                    "Automatically start playing song on start".to_owned(),
+                    self.config.get_autoplay(),
+                    |val| Msg::Conf(ConfMsg::Autoplay(val))
+                ),
+                toggler(
+                    "Play songs without gap between them".to_owned(),
+                    self.config.get_gapless(),
+                    |val| Msg::Conf(ConfMsg::Gapless(val))
+                ),
+                text("Hotkeys:").height(22).style(Text::Normal),
+                toggler(
+                    "Enable hotkeys".to_owned(),
+                    self.config.get_enable_hotkeys(),
+                    |val| Msg::Conf(ConfMsg::EnableHotkeys(val))
+                ),
+            ]
+            .width(Length::Fill)
+            .spacing(2)
+            .padding(3),
+        )
         .into()
     }
 
